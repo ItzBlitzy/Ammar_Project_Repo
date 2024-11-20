4ca6bbdd67a08e52f5d1bce3b925219a4cc995f1
ItzBlitzy <ammarsufi2004@gmail.com>
2024-11-20 18:43:38 +0800

    Add project files.

---
+++ Ammar_Project/Controllers/BookingsController.cs
@@ -0,0 +1,94 @@
+﻿using Ammar_Project.Data;
+using Ammar_Project.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ammar_Project.Controllers
+{
+    [Route("api/[controller]/{action}")]
+    [ApiController]
+    public class BookingsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BookingsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Bookings
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_context.Bookings);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int? id)
+        {
+            var bookings = _context.Bookings.FirstOrDefault(e => e.BookingID == id);
+            if (bookings == null)
+                return Problem(detail:"Booking with id " + id + " is not found.", statusCode:404);
+
+            return Ok(bookings);
+        }
+
+        [HttpGet("{status}")]
+        public IActionResult GetByStatus(string? status = "All")
+        {
+            switch (status.ToLower())
+            {
+                case "all":
+                    return Ok(_context.Bookings);
+                case "pending":
+                    return Ok(_context.Bookings.Where(e => e.BookingStatus.ToLower() == "pending"));
+                case "approved":
+                    return Ok(_context.Bookings.Where(e => e.BookingStatus.ToLower() == "approved"));
+                case "rejected":
+                    return Ok(_context.Bookings.Where(e => e.BookingStatus.ToLower() == "rejected"));
+                default:
+                    return Problem(detail: "Booking with status " + status + " is not found.", statusCode: 404);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post(Booking booking)
+        {
+            _context.Bookings.Add(booking);
+            _context.SaveChanges();
+
+            return CreatedAtAction("GetAll", new { id = booking.BookingID }, booking);
+        }
+
+        [HttpPut]
+        public IActionResult Put(int? id, Booking booking)
+        {
+            var entity = _context.Bookings.FirstOrDefault(e => e.BookingID == id);
+            if (entity == null)
+                return Problem(detail: "Booking with id " + id + " is not found.", statusCode: 404);
+
+            entity.FacilityDescription = booking.FacilityDescription;
+            entity.BookingDateFrom = booking.BookingDateFrom;
+            entity.BookingDateTo = booking.BookingDateTo;
+            entity.BookedBy = booking.BookedBy;
+            entity.BookingStatus = booking.BookingStatus;
+
+            _context.SaveChanges();
+
+            return Ok(entity);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int? id, Booking booking)
+        {
+            var entity = _context.Bookings.FirstOrDefault(e => e.BookingID == id);
+            if (entity == null)
+                return Problem(detail: "Booking with id " + id + " is not found.", statusCode: 404);
+
+            _context.Bookings.Remove(entity);
+            _context.SaveChanges();
+
+            return Ok(entity);
+        }
+    }
+}
---
+++ Ammar_Project/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Ammar_Project.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ammar_Project.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) 
+        { 
+        }
+
+        public DbSet<Booking>? Bookings { get; set; }
+    }
+}
